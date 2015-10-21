7d8746b1fcbcb434d89caf35f26639b9bffe3f85
Jonas I. Liechti <jonas.i.liechti@protonmail.ch>
2015-10-21 02:29:33 +0200

    updated RateDistribution from dev_alex

--- endemic/nw_spread/RateDistribution.py
+++ endemic/nw_spread/RateDistribution.py
@@ -1,44 +1,78 @@
-from numpy import vectorize, random, apply_along_axis, array, float64
+from numpy import vectorize, array, float64
+from scipy.stats import expon, norm, lomax
 from Queue import Empty
 from Queue import Queue as SimpleQueue
 
 #this is the time it takes if a rate of 0 is given
-MAX_LIM = 100000
+MAX_LIM = 10000
 
 
-def no_mut(dummy, length):
-    return array([MAX_LIM] * length, dtype=float64)
+def no_mut(loc=None, scale=None, size=10):
+    return array([MAX_LIM] * size, dtype=float64)
 
 
 class Distro(object):
-    def __init__(self, scale, pre=10):
-        """
-        This class holds a queue of times drawn from an exponential 
-            distribution with a specified scale.
-        
-        Arguments:
-        
-            - scale: The scale parameter for the exponential distribution.
-            - pre: Predefined size of the queue. Default=10
-        """
+    """
+    This class holds a queue of random times drawn from a given distribution with a specified scale.
+    
+    Arguments:
+    
+        - scale: The scale parameter for the exponential distribution. (default=1)
+        - pre: Predefined size of the queue. (default=10)
+        - loc: location of the distribution (default=0)
+        - alpha: power law exponent (default=1), only for the lomax distribution
+        - distribution type: string of the name of a distribution from scipy.stats
+            "exponential" (default) with parameter scale:
+                pdf(x, scale) = (1/scale) * exp(-x/scale)
+                
+            "gaussian":
+                pdf(x, scale, loc) = |(1/scale) * norm((x-loc)/scale)|
+                note : returns the absolute value
+                
+            "lomax" power-law distribution:
+                pdf(x, alpha) = alpha / (1+x)**(alpha+1)
+                for x >= 0, alpha > 0
+                the minimum value is controlled by `loc`
+            
+    """
+    def __init__(self, scale=1, pre=10, loc=0, alpha=1, distribution_type="exponential"):
+       
         self.scale = scale
+        self.loc = loc
         self.pre = pre
+        self.alpha = alpha
         self.queue = SimpleQueue(maxsize=pre + 1)
         self.v_put = vectorize(self.queue.put_nowait)
+        self.distribution_type = distribution_type
+        
+        
         #the exponential dist is not defined for a rate of 0
         #therefore if the rate is 0 (scale is None then) huge times are set
-        if self.scale in [None, 0]:
-            self.scale = 0
+        if self.scale is None or self.scale == 0:
             self.draw_fct = no_mut
         else:
-            self.draw_fct = random.exponential
+            if distribution_type == "exponential":                
+                self.draw_fct = expon.rvs
+            elif distribution_type == "gaussian":
+                self.draw_fct = norm.rvs
+            elif distribution_type == "lomax":
+                self.draw_fct = lomax.rvs
+            else:
+                raise self.DistributionTypeError('This distribution type is not implemented yet.')
+                
         #fillup the queue
         self.fillup()
         # there was: (new version compatible with pickeling see method below)
         self.v_get = vectorize(self.get_val)
 
+    class DistributionTypeError(Exception):
+        pass
+    
     def fillup(self):
-        self.v_put(self.draw_fct(self.scale, self.pre))
+        if self.distribution_type == "lomax":
+            self.v_put(abs(self.draw_fct(self.alpha, loc=self.loc, scale=self.scale, size=self.pre)))
+        else:
+            self.v_put(abs(self.draw_fct(loc=self.loc, scale=self.scale, size=self.pre)))
         return 0
 
     def get_val(self, a=None):
@@ -72,21 +106,12 @@ class Distro(object):
         return d
 
     # to load the pickled event list back into a priority queue
-    # TO DO: oh boy this is some horrible patch
     def __setstate__(self, d):
         if 'simple_queue_list' in d:
             event_queue_list = d.pop('simple_queue_list')
-            d['queue'] = SimpleQueue(maxsize=d['pre'] + 1)
+            d['queue'] = SimpleQueue()
             while len(event_queue_list):
                 d['queue'].put_nowait(event_queue_list.pop())
+        d['v_put'] = vectorize(d['queue'].put_nowait)
         self.__dict__.update(d)
-        self.__dict__['v_put'] = vectorize(self.queue.put_nowait)
-        #d['v_put'] = vectorize(d['queue'].put_nowait)
-        #self.__dict__.update(d)
         self.__dict__['v_get'] = vectorize(self.get_val)
-        if self.scale is None:
-            self.scale = 0
-            self.queue = SimpleQueue(maxsize=self.pre + 1)
-            self.v_put = vectorize(self.queue.put_nowait)  # this is specific to the queue, thus reinit here
-            self.draw_fct = no_mut
-            self.fillup()
