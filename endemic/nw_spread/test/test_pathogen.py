40fb0df5b404035f00715b905fe9f9b0af25980b
Jonas I Liechti <jonas.i.liechti@protonmail.ch>
2020-01-07 15:26:13 +0100

    more testing

--- endemic/nw_spread/test/test_pathogen.py
+++ endemic/nw_spread/test/test_pathogen.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestStrain(unittest.TestCase):
 
-    def test_to_sec(self):
+    def test_rate_conversion(self):
 
         string_sec = {
                 '22sec': 22.,
@@ -13,12 +13,14 @@ class TestStrain(unittest.TestCase):
                 '1min': 60.,
                 '1.6min': 1.6*60,
                 '1.6h': 1.6*60*60,
-                '3d': 3*24*60*60,
+                '3d': 3.*24*60*60,
                 '3.1d': 3.1*24*60*60,
                 }
 
         for s in string_sec:
             self.assertTrue(string_sec[s] == Strain.to_sec(s))
+        for s in string_sec:
+            self.assertTrue(1/string_sec[s] == Strain.to_rate(s))
 
 
 if __name__ == '__main__':
